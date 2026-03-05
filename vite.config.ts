9a23d392c0aedbe467751d6880eda204a30d00f0
Dusty McLean <dusty@Dustys-MacBook-Pro-2.local>
2026-03-05 12:23:13 -0600

    Fix: Add deployment workflow and set base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+  return {
+    base: '/SAPA/',
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
